f8f2e39ada8421d70b0c96ee610d353ddb8e2a95
revinto <jakub.gornikk@gmail.com>
2023-12-21 00:05:39 +0100

    resume download button

--- app/content/ui/ButtonResume.tsx
+++ app/content/ui/ButtonResume.tsx
@@ -1,14 +1,16 @@
 import DownloadIcon from "../../components/svg/DownloadIcon";
+import Link from "next/link";
 
 const ButtonResume = () => {
   return (
-    <button
-      type="button"
+    <Link
       className="group fixed right-8 flex cursor-pointer items-center gap-2 rounded-lg bg-lightPink px-4 py-[14px] text-[14px] font-semibold text-darkPurple shadow-lg duration-300 hover:bg-yellowishWhite"
+      download="Jakub-Górnik-CV-eng.pdf"
+      href="/cv/cv-eng.pdf"
     >
       CV
       <DownloadIcon />
-    </button>
+    </Link>
   );
 };
 
