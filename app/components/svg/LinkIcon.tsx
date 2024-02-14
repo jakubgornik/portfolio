52b7ff9be95b11148b1a14639b6afec0435bc696
revinto <jakub.gornikk@gmail.com>
2024-02-14 22:57:36 +0100

    svg props fix

--- app/components/svg/LinkIcon.tsx
+++ app/components/svg/LinkIcon.tsx
@@ -2,7 +2,7 @@ const LinkIcon = () => {
   return (
     <svg
       className="h-[40px] w-[40px] cursor-pointer fill-none stroke-lightPink duration-300 hover:stroke-yellowishWhite"
-      stroke-width="2"
+      strokeWidth="2"
     >
       <title>External Link</title>
       <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
