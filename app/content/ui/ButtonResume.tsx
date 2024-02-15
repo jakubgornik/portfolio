5ebe3f78d5086cc2f33ded23f4bc62d7e776cc0d
revinto <jakub.gornikk@gmail.com>
2024-02-15 20:27:22 +0100

    updated cv

--- app/content/ui/ButtonResume.tsx
+++ app/content/ui/ButtonResume.tsx
@@ -5,7 +5,7 @@ const ButtonResume = () => {
   return (
     <Link
       className="flex cursor-pointer items-center gap-2 rounded-lg bg-lightPink px-10 py-2 text-[14px] font-semibold text-darkPurple shadow-lg duration-300 hover:bg-yellowishWhite sm:fixed sm:right-8 sm:px-4 sm:py-[14px] "
-      href="/cv/cv-eng.pdf"
+      href="/cv/cv-english.pdf"
       target="_blank"
     >
       CV
